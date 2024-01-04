35cab190ef361e95e8c5b5c5dc89063e16fa5bd0
Iris Chou <iris.w.chou@gmail.com>
2024-01-03 21:26:58 -0800

    window setup

---
+++ src/Core/DrawingSurface.java
@@ -0,0 +1,28 @@
+package Core;
+
+import processing.core.PApplet;
+import java.util.*;
+import Screens.*;
+
+public class DrawingSurface extends PApplet {
+
+	private ArrayList<Screen> screens;
+	private Screen currentScreen;
+	
+	public DrawingSurface() {
+		this.screens = new ArrayList<Screen>();
+	}
+	
+	public void setup() {
+		for (Screen screen : screens)
+			screen.setup();
+	}
+	
+	public void mousePressed() {
+		currentScreen.mousePressed();
+	}
+	
+	public void keyPressed() {
+		currentScreen.keyPressed();
+	}
+}
--- src/Core/Main.java
+++ src/Core/Main.java
@@ -1,9 +1,29 @@
 package Core;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+
 public class Main {
 	
 	public static void main(String args[]) {
+		DrawingSurface drawing = new DrawingSurface();
+		PApplet.runSketch(new String[]{""}, drawing);
+		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		drawing.windowMove(0, 0);
+		JFrame window = (JFrame)canvas.getFrame();
 		
+		window.setSize(1600, 900);
+		window.setMinimumSize(new Dimension(100,100));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+
+		window.setVisible(true);
+		canvas.requestFocus();
 	}
 	
 }
