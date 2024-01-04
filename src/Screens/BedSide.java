29d06675671b706d1e19cd0325b794e48779a1d8
Monica Zhang <108095408+monica410@users.noreply.github.com>
2024-01-03 22:14:54 -0800

    Update BedSide.java

--- src/Screens/BedSide.java
+++ src/Screens/BedSide.java
@@ -1,5 +1,25 @@
 package Screens;
 
-public class BedSide {
+import Core.DrawingSurface;
+
+public class BedSide extends Screen {
+
+	private DrawingSurface surface;
+	
+	public BedSide(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void draw() {
+
+		surface.pushStyle();
+		
+		surface.background(150,150,100);
+		
+		
+		
+		surface.popStyle();
+	}
 
 }
