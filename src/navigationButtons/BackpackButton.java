da340b476c26c75c4f7e2b77ba5e2ddfc5264e17
Monica Zhang <108095408+monica410@users.noreply.github.com>
2024-01-04 22:27:19 -0800

    buttons

--- src/navigationButtons/BackpackButton.java
+++ src/navigationButtons/BackpackButton.java
@@ -11,10 +11,10 @@ public class BackpackButton extends JButton{
 	
 	public BackpackButton(JPanel p, ImageIcon img) {
 		super(img);
-		this.setBounds(875, 700, 60, 60);
-//		this.setOpaque(false);
-//		this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-//		this.setBorderPainted(false);
+		this.setBounds(875, 20, 65, 65);
+		this.setBackground(new Color(220,190,140));
+		this.setOpaque(true);
+		this.setBorderPainted(false);
 		this.addActionListener((ActionListener) p);
 		p.add(this);
 	}
