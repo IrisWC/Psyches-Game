f6560ad0a23de35b2cd3e96224a9457762b8014f
Iris Chou <iris.w.chou@gmail.com>
2024-01-04 22:03:27 -0800

    Update BackpackButton.java

--- src/navigationButtons/BackpackButton.java
+++ src/navigationButtons/BackpackButton.java
@@ -1,5 +1,6 @@
 package navigationButtons;
 
+import java.awt.Color;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
@@ -10,10 +11,10 @@ public class BackpackButton extends JButton{
 	
 	public BackpackButton(JPanel p, ImageIcon img) {
 		super(img);
-		this.setBounds(875, 700, 50, 50);
+		this.setBounds(875, 700, 60, 60);
 		this.setOpaque(false);
-		this.setContentAreaFilled(false);
-		this.setBorderPainted(false);
+		this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+//		this.setBorderPainted(false);
 		this.addActionListener((ActionListener) p);
 		p.add(this);
 	}
