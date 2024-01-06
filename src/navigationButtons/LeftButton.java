2281c3569886a2b924c82280943430e500b502ec
Iris Chou <iris.w.chou@gmail.com>
2024-01-05 21:11:57 -0800

    add arrows

--- src/navigationButtons/LeftButton.java
+++ src/navigationButtons/LeftButton.java
@@ -1,8 +1,10 @@
 package navigationButtons;
 
 import java.awt.Color;
+import java.awt.Image;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -12,6 +14,9 @@ public class LeftButton extends JButton{
 	
 	public LeftButton(JPanel p) {
 		this.setBounds(0, 0, 100, Game.HEIGHT);
+		ImageIcon arrowIcon = new ImageIcon("img/leftarrow.png");
+		Image arrowModified = arrowIcon.getImage().getScaledInstance(48, 80, Image.SCALE_SMOOTH);
+		this.setIcon(new ImageIcon(arrowModified));
 		this.setBackground(new Color(50,47,48));
 		this.setOpaque(true);
 		this.setBorderPainted(false);
