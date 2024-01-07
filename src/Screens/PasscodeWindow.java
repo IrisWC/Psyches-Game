b60ebf9b5aaf0486073e7b755e8121b453a4f68c
Iris Chou <iris.w.chou@gmail.com>
2024-01-06 21:52:44 -0800

    Update PasscodeWindow.java

--- src/Screens/PasscodeWindow.java
+++ src/Screens/PasscodeWindow.java
@@ -2,6 +2,7 @@ package Screens;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 import javax.swing.JButton;
@@ -37,6 +38,7 @@ public class PasscodeWindow extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == submitButton) {
 //			this.dispatchEventToSelf(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+			this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
 		}
 	}
 }
